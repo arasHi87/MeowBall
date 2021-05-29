c722ed93bd3fa915114239e52009177df0ebec52
arashi87 <arasi27676271@gmail.com>
2021-05-30 05:18:06 +0800

    feat(utils): add new utils function

--- src/main/base/Utils.java
+++ src/main/base/Utils.java
@@ -1,8 +1,12 @@
 package main.base;
 
+import javax.imageio.ImageIO;
 import javax.swing.ImageIcon;
 import main.Content;
-import java.awt.Image;
+import java.awt.*;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
 
 public abstract class Utils {
     /**
@@ -12,6 +16,34 @@ public abstract class Utils {
      * @return
      */
     public static Image getImage(String imageName) {
-        return new ImageIcon(Content.RESOURCE_PATH + "images/" + imageName).getImage();
+        return new ImageIcon(getImagePath(imageName)).getImage();
+    }
+
+    public static ImageIcon getResizeImage(String imageName, int width, int height) {
+        BufferedImage image = null;
+        try {
+            image = ImageIO.read(new File(getImagePath(imageName)));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        Image resizeImage = image.getScaledInstance(width, height, Image.SCALE_SMOOTH);
+        return new ImageIcon(resizeImage);
+    }
+
+    public static String getImagePath(String imageName) {
+        return Content.RESOURCE_PATH + "images/" + imageName;
+    }
+
+    public static String getFontPath(String fontName) {
+        return Content.RESOURCE_PATH + "font/" + fontName;
+    }
+
+    public static Font getFont(String fontName, int fontSize) throws FontFormatException, IOException {
+        Font font = null;
+        font = Font.createFont(Font.TRUETYPE_FONT, new File(Utils.getFontPath("8bit16.ttf")));
+        font = font.deriveFont(java.awt.Font.PLAIN, fontSize);
+
+        return font;
     }
 }
