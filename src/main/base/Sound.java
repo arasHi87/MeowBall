69142e9325ab23cf503392fb4a43e14f61cf0abf
arashi87 <arasi27676271@gmail.com>
2021-06-04 16:39:45 +0800

    feat(sound): add volume control to let bgm volume smaller

--- src/main/base/Sound.java
+++ src/main/base/Sound.java
@@ -8,12 +8,28 @@ public class Sound {
     // Each sound effect has its own clip, loaded with its own sound file.
     private Clip clip;
 
+    public float getVolume() {
+        FloatControl gainControl = (FloatControl) clip.getControl(FloatControl.Type.MASTER_GAIN);
+        return (float) Math.pow(10f, gainControl.getValue() / 20f);
+    }
+
+    public void setVolume(float volume) {
+        if (volume >= 0f || volume <= 1f) {
+            FloatControl gainControl = (FloatControl) clip.getControl(FloatControl.Type.MASTER_GAIN);
+            gainControl.setValue(20f * (float) Math.log10(volume));
+        }
+    }
+
     public void play(String soundName, boolean loop) {
         try {
             File file = new File(Utils.getSoundPath(soundName));
             AudioInputStream audioStream = AudioSystem.getAudioInputStream(file);
             clip = AudioSystem.getClip();
             clip.open(audioStream);
+
+            if (soundName == "game" || soundName == "start") {
+                setVolume(0.5f);
+            }
 
             if (loop == true) {
                 clip.loop(Clip.LOOP_CONTINUOUSLY);
