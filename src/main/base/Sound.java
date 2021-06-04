77162f09d909a3308670d90d5f423dbc36cd06be
arashi87 <arasi27676271@gmail.com>
2021-06-04 15:21:02 +0800

    feat(sound): add getFileName function

--- src/main/base/Sound.java
+++ src/main/base/Sound.java
@@ -5,39 +5,42 @@ import java.io.IOException;
 import javax.sound.sampled.*;
 
 public class Sound {
-
     // Each sound effect has its own clip, loaded with its own sound file.
     private Clip clip;
-    private String fileName;
+
+    private String getFileName(String soundName) {
+        String fileName = "";
+        switch (soundName) {
+            case "smash":
+                fileName = "src/main/resources/sounds/fire_sound.wav";
+                break;
+            case "cheer":
+                fileName = "src/main/resources/sounds/cheer.wav";
+                break;
+            case "hit":
+                fileName = "src/main/resources/sounds/hit.wav";
+                break;
+            case "select":
+                fileName = "src/main/resources/sounds/select.wav";
+                break;
+            case "start":
+                fileName = "src/main/resources/sounds/start.wav";
+                break;
+            case "fail":
+                fileName = "src/main/resources/sounds/fail.wav";
+                break;
+            case "game":
+                fileName = "src/main/resources/sounds/game.wav";
+                break;
+            default:
+                break;
+        }
+        return fileName;
+    }
 
     public void play(String soundName, boolean loop) {
         try {
-            switch (soundName) {
-                case "smash":
-                    fileName = "src/main/resources/sounds/fire_sound.wav";
-                    break;
-                case "cheer":
-                    fileName = "src/main/resources/sounds/cheer.wav";
-                    break;
-                case "hit":
-                    fileName = "src/main/resources/sounds/hit.wav";
-                    break;
-                case "select":
-                    fileName = "src/main/resources/sounds/select.wav";
-                    break;
-                case "start":
-                    fileName = "src/main/resources/sounds/start.wav";
-                    break;
-                case "fail":
-                    fileName = "src/main/resources/sounds/fail.wav";
-                    break;
-                case "game":
-                    fileName = "src/main/resources/sounds/game.wav";
-                    break;
-                default:
-                    break;
-            }
-            File file = new File(fileName);
+            File file = new File(getFileName(soundName));
             AudioInputStream audioStream = AudioSystem.getAudioInputStream(file);
             clip = AudioSystem.getClip();
             clip.open(audioStream);
